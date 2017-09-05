d3f316516ebc9af958ecfe0b2c64379e1996b53c
Sandeep <sjnews@gmail.com>
2017-09-05 12:57:56 -0400

    clases and ids

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ $("document").ready(function () {
 	// $("p:last").css("border", "4px solid red");
 	// $("p:even").css("border", "4px solid red");
 	// $("p:odd").css("border", "4px solid red");
-	// $("div p").css("border", "3px solid red");
-	// $("li:first").css("border", "3px solid red");
+	// $("div p").css("border", "4px solid red");
+	// $("li:first").css("border", "4px solid red");
 
 
 
 	//TARGETTING CLASSES AND IDs
+	// $("p[class]").css("border", "4px solid red");
+	// $("p[class=kitten]").css("border", "4px solid red");
+	// $("p[id=black-kitten]").css("border", "4px solid red");
+	$("p[id^=black]").css("border", "4px solid red");
+
 	// $(".lesson p").css("border", "3px solid red");
 	// $(".lesson li").css("border", "3px solid red");
 	// $(".lesson li:first").css("border", "3px solid red");
