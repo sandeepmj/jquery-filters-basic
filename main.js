69a0baa5870cf1fa3c47a696e923f1838c12194b
Sandeep <sjnews@gmail.com>
2017-09-05 10:14:55 -0400

    lesson code

---
+++ main.js
@@ -0,0 +1,31 @@
+$("document").ready(function () {
+
+	//TARGETTING TAGS
+
+	// $("p:first").css("border", "4px solid red");
+	// $("p:last").css("border", "4px solid red");
+	// $("p:even").css("border", "4px solid red");
+	// $("p:odd").css("border", "4px solid red");
+	// $("div p").css("border", "3px solid red");
+	// $("li:first").css("border", "3px solid red");
+
+
+
+	//TARGETTING CLASSES AND IDs
+	// $(".lesson p").css("border", "3px solid red");
+	// $(".lesson li").css("border", "3px solid red");
+	// $(".lesson li:first").css("border", "3px solid red");
+	// $(".lesson li:last").css("border", "3px solid red");
+	// $(".lesson li:gt(1)").css("border", "3px solid red");
+
+
+	//$("#example p:last").css("border", "3px solid red");
+	//$("#example p:even").css("border", "3px solid red");
+	//$("#example p:odd").css("border", "3px solid red");
+	//$("#example .a:first").css("border", "3px solid red");
+	//$("#example .b:even").css("border", "3px solid red");
+	//$("#example p:gt(1)").css("border","3px solid red");
+	//$("#example p:not(p:eq(2))").css("border", "3px solid red");
+
+
+});
